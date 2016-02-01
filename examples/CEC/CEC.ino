8daa2c7d2c3863ad98d2a4faba611f7dede94a72
Florian Echtler <floe@butterbrot.org>
2016-02-01 08:35:13 +0100

    hackish example, tested/works with old Philips 40PFL5605K

--- examples/CEC/CEC.ino
+++ examples/CEC/CEC.ino
@@ -3,34 +3,37 @@
 #define IN_LINE 2
 #define OUT_LINE 3
 
-CEC_Device device(0x1000);
+class MyCEC: public CEC_Device {
+  public:
+    MyCEC(int physAddr): CEC_Device(physAddr,IN_LINE,OUT_LINE) { }
+    void OnReceive(int source, int dest, unsigned char* buffer, int count) {
+      unsigned char frame86[3] = { 0x82, 0x10, 0x00 };
+      unsigned char frame83[4] = { 0x84, 0x10, 0x00, 0x04 }; // FIXME: physical address hardcoded
+      unsigned char frame8F[2] = { 0x90, 0x00 };
+      unsigned char frame9F[2] = { 0x9E, 0x04 };
+      unsigned char frame46[5] = { 0x47, 'H','T','P','C' }; // FIXME: name hardcoded
+      CEC_Device::OnReceive(source,dest,buffer,count);
+      if (count == 0) return;
+      switch (buffer[0]) {
+        case 0x36: DbgPrint("standby\n"); break;
+        case 0x83: DbgPrint("report phys addr\n"); TransmitFrame(0x0F,frame83,sizeof(frame83)); break; // report physical address
+        case 0x86: DbgPrint("report stream status\n"); if (buffer[1] == 0x10 && buffer[2] == 0x00) TransmitFrame(0x0F,frame86, sizeof(frame86)); break; // report stream status
+        case 0x8F: DbgPrint("report power status\n"); TransmitFrame(0x00,frame8F,sizeof(frame8F)); break; // report power status  
+        case 0x9F: DbgPrint("report CEC version\n"); TransmitFrame(0x00,frame9F,sizeof(frame9F)); break; // report CEC version 
+        case 0x46: DbgPrint("report OSD name\n"); TransmitFrame(0x00,frame46,sizeof(frame46)); break; // report OSD name
+        default:   break;
+      }
+    }
+};
 
-bool XX_GetLineState()
-{
-  int state = digitalRead(IN_LINE);
-  return state == LOW;
-}
-
-void XX_SetLineState(CEC_Device* device, bool state)
-{
-  digitalWrite(OUT_LINE, state?LOW:HIGH);
-  // give enough time for the line to settle before sampling
-  // it
-  delayMicroseconds(50);
-  device->_lastLineState2 = XX_GetLineState();
-}
+MyCEC device(0x1000);
 
 void setup()
 {
-  pinMode(OUT_LINE, OUTPUT);
-  pinMode(IN_LINE, INPUT);
-
-  digitalWrite(OUT_LINE, LOW);
-  delay(200);
-
   Serial.begin(115200);
+  device.Init();
   //device.MonitorMode = true;
-  device.Promiscuous = true;
+  //device.Promiscuous = true;
   device.Initialize(CEC_LogicalDevice::CDT_PLAYBACK_DEVICE);
 }
 
@@ -44,12 +47,19 @@ void loop()
     switch (c)
     {
       case '1':
-        buffer[0] = 0x36;
-        device.TransmitFrame(4, buffer, 1);
+        // request vendor ID
+        buffer[0] = 0x8C;
+        device.TransmitFrame(0, buffer, 1);
         break;
+      case '2':
+        // ping
+        device.TransmitFrame(0, buffer, 0);
+      case '3':
+        buffer[0] = 0x8E;
+        buffer[1] = 0x01;
+        device.TransmitFrame(0,buffer,2);
     }
   }
   device.Run();
 }
-
 
