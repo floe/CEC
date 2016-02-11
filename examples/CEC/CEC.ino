f8d344e92b5af57cf090ef4c2c496df2b3402412
Florian Echtler <floe@butterbrot.org>
2016-02-11 11:43:56 +0100

    update/cleanup example

--- examples/CEC/CEC.ino
+++ examples/CEC/CEC.ino
@@ -1,20 +1,27 @@
 #include "CEC_Device.h"
 
+// TODO: control the hot-plug detect pin (19) via software?
 #define IN_LINE 2
 #define OUT_LINE 3
 
 #define report(X) do { DbgPrint("report " #X "\n"); report ## X (); } while (0)
 
+#define phy1 ((_physicalAddress >> 8) & 0xFF)
+#define phy2 ((_physicalAddress >> 0) & 0xFF)
+
 class MyCEC: public CEC_Device {
   public:
     MyCEC(int physAddr): CEC_Device(physAddr,IN_LINE,OUT_LINE) { }
     
-    void reportPhysAddr()    { unsigned char frame[4] = { 0x84, 0x10, 0x00, 0x04 }; TransmitFrame(0x0F,frame,sizeof(frame)); } // FIXME: physical address hardcoded}
-    void reportStreamState() { unsigned char frame[3] = { 0x82, 0x10, 0x00 };       TransmitFrame(0x0F,frame,sizeof(frame)); } // FIXME: physical address hardcoded}
-    void reportPowerState()  { unsigned char frame[2] = { 0x90, 0x00 };             TransmitFrame(0x00,frame,sizeof(frame)); } // report power on
-    void reportCECVersion()  { unsigned char frame[2] = { 0x9E, 0x04 };             TransmitFrame(0x00,frame,sizeof(frame)); } // report CEC v1.3a
+    void reportPhysAddr()    { unsigned char frame[4] = { 0x84, phy1, phy2, 0x04 }; TransmitFrame(0x0F,frame,sizeof(frame)); } // report physical address
+    void reportStreamState() { unsigned char frame[3] = { 0x82, phy1, phy2 };       TransmitFrame(0x0F,frame,sizeof(frame)); } // report stream state (playing)
+    
+    void reportPowerState()  { unsigned char frame[2] = { 0x90, 0x00 };             TransmitFrame(0x00,frame,sizeof(frame)); } // report power state (on)
+    void reportCECVersion()  { unsigned char frame[2] = { 0x9E, 0x04 };             TransmitFrame(0x00,frame,sizeof(frame)); } // report CEC version (v1.3a)
+    
     void reportOSDName()     { unsigned char frame[5] = { 0x47, 'H','T','P','C' };  TransmitFrame(0x00,frame,sizeof(frame)); } // FIXME: name hardcoded
     void reportVendorID()    { unsigned char frame[4] = { 0x87, 0x00, 0xF1, 0x0E }; TransmitFrame(0x00,frame,sizeof(frame)); } // report fake vendor ID
+    // TODO: implement menu status query (0x8D) and report (0x8E,0x00)
     
     void handleKey(unsigned char key) {
       switch (key) {
@@ -24,8 +31,8 @@ class MyCEC: public CEC_Device {
         case 0x03: Keyboard.press(KEY_LEFT_ARROW); break;
         case 0x04: Keyboard.press(KEY_RIGHT_ARROW); break;
         case 0x0D: Keyboard.press(KEY_ESC); break;
-        case 0x4C: Keyboard.press(KEY_PAGE_DOWN); break;
-        case 0x4B: Keyboard.press(KEY_PAGE_UP); break;
+        case 0x4B: Keyboard.press(KEY_PAGE_DOWN); break;
+        case 0x4C: Keyboard.press(KEY_PAGE_UP); break;
         case 0x53: Keyboard.press(KEY_HOME); break;
       }
     }
@@ -33,11 +40,16 @@ class MyCEC: public CEC_Device {
     void OnReceive(int source, int dest, unsigned char* buffer, int count) {
       if (count == 0) return;
       switch (buffer[0]) {
+        
         case 0x36: DbgPrint("standby\n"); break;
+        
         case 0x83: report(PhysAddr); break;
-        case 0x86: if (buffer[1] == 0x10 && buffer[2] == 0x00) report(StreamState); break; // report stream status
+        case 0x86: if (buffer[1] == phy1 && buffer[2] == phy2)
+                   report(StreamState); break;
+        
         case 0x8F: report(PowerState); break;
         case 0x9F: report(CECVersion); break;  
+        
         case 0x46: report(OSDName);    break;
         case 0x8C: report(VendorID);   break;
         
@@ -49,6 +61,7 @@ class MyCEC: public CEC_Device {
     }
 };
 
+// TODO: set physical address via serial (or even DDC?)
 MyCEC device(0x1000);
 
 void setup()
@@ -62,6 +75,8 @@ void setup()
 
 void loop()
 {
+  // FIXME: does it still work without serial connected?
+  // try to toggle LED e.g.
   if (Serial.available())
   {
     unsigned char c = Serial.read();
@@ -69,14 +84,14 @@ void loop()
     
     switch (c)
     {
-      case '1':
+      case 'v':
         // request vendor ID
         buffer[0] = 0x8C;
         device.TransmitFrame(0, buffer, 1);
         break;
-      case '2':
-        // ping
-        device.TransmitFrame(0, buffer, 0);
+      case 'p':
+        // toggle promiscuous mode
+        device.Promiscuous = !(device.Promiscuous);
         break;
     }
   }
